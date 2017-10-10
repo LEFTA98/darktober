063d976bdc182d05857718818de29e3836e67493
mathomas <thomayinma@gmail.com>
2017-10-09 23:44:29 -0400

    Added basic background, block texture, temp player texture, player movement script

---
+++ Darktober/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rigidbody;
+
+    public float horizSpeed = 1;
+    public float vertSpeed = 1;
+
+    // Use this for initialization
+    void Start () {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Horizontal movement
+        float horizontal = 0f;
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        //Vertical movement
+        float vertical = 0f;
+        if (Input.GetButtonDown("Jump"))
+        {
+            vertical = 5f;
+        }
+        else {
+
+            vertical = 0f;
+
+        }
+
+        rigidbody.velocity = new Vector2(horizontal * horizSpeed, vertical * vertSpeed + rigidbody.velocity.y);
+		
+	}
+}
